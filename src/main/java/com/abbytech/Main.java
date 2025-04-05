bfc806700bb46d3788053c40b152b637922f3a87
abbytech <khraimabduallah@gmail.com>
2025-04-05 20:02:57 +0300

    decode multi-key presses, add shutdown key combo (FN+HOME)

--- src/main/java/com/abbytech/Main.java
+++ src/main/java/com/abbytech/Main.java
@@ -5,57 +5,69 @@ import uk.co.bithatch.linuxio.EventCode;
 import uk.co.bithatch.linuxio.InputDevice;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws DecoderException, IOException {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try {
-                USB.reset();
-            } catch (DecoderException e) {
-                throw new RuntimeException(e);
-            }
-        }));
+    private static InputDevice virtualInputDevice;
 
-        InputDevice dev = new InputDevice("Razer WASD", (short) 0x1234, (short) 0x5678);
+    public static void main(String[] args) throws DecoderException, IOException {
+        Runtime.getRuntime().addShutdownHook(new Thread(Main::shutdown));
+
+        virtualInputDevice = new InputDevice("Razer WASD", (short) 0x1234, (short) 0x5678);
 
         /*dev.getCapabilities().add(EventCode.ABS_GAS);
         dev.getCapabilities().add(EventCode.ABS_BRAKE);
         dev.getCapabilities().add(EventCode.ABS_WHEEL);*/
-        dev.getCapabilities().add(EventCode.ABS_Y);
-        dev.getCapabilities().add(EventCode.ABS_X);
-        dev.open();
+        virtualInputDevice.getCapabilities().add(EventCode.ABS_Y);
+        virtualInputDevice.getCapabilities().add(EventCode.ABS_X);
+        virtualInputDevice.open();
         USB.listen(event -> {
-            String key = event.getKey();
-            Integer value = event.getValue();
-            if (key == null) {
-                return;
-            }
-            EventCode eventCode;
-            switch (key) {
-                case "W":
-                case "S":
-                    eventCode = EventCode.ABS_Y;
-                    if (key.equals("S")) {
-                        value = value * -1;
-                    }
-                    break;
-                case "A":
-                case "D":
-                    eventCode = EventCode.ABS_X;
-                    if (key.equals("A")) {
-                        value = value * -1;
-                    }
-                    break;
-                default:
-                    System.out.println("unknown key");
+            List<USB.EventKey> eventParts = event.getEventParts();
+            if (eventParts.size() == 1) {
+                USB.EventKey eventKey = eventParts.get(0);
+                USB.Key key = eventKey.getKey();
+                Integer value = eventKey.getValue();
+                if (key == null) {
                     return;
+                }
+                EventCode eventCode;
+                switch (key) {
+                    case W:
+                    case S:
+                        eventCode = EventCode.ABS_Y;
+                        if (key.equals(USB.Key.S)) {
+                            value = value * -1;
+                        }
+                        break;
+                    case A:
+                    case D:
+                        eventCode = EventCode.ABS_X;
+                        if (key.equals(USB.Key.A)) {
+                            value = value * -1;
+                        }
+                        break;
+                    default:
+                        System.out.println("unknown key");
+                        return;
+                }
+                try {
+                    virtualInputDevice.emit(new InputDevice.Event(eventCode, value));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
-            try {
-                dev.emit(new InputDevice.Event(eventCode, value));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+
         });
-        dev.close();
+    }
+
+    private static void shutdown() {
+        try {
+            USB.reset();
+            virtualInputDevice.close();
+            System.exit(0);
+        } catch (Exception e) {
+            //ignored
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/abbytech/USB.java
+++ src/main/java/com/abbytech/USB.java
@@ -7,10 +7,7 @@ import org.usb4java.*;
 
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -20,26 +17,35 @@ public class USB {
 
     private static final int HID_DATA_LENGTH = 48;
 
-    private static final Map<String, String> keyCodeMap = new HashMap<>();
+    private static final Map<String, Key> keyCodeMap = new HashMap<>();
+    private static final int initialCapacity = 15;
     private static DeviceHandle deviceHandle;
 
-    static {
-        keyCodeMap.put("12", "W");
-        keyCodeMap.put("1f", "A");
-        keyCodeMap.put("20", "S");
-        keyCodeMap.put("21", "D");
+
+    enum Key {
+        W, A, S, D, FN, HOME, UNMAPPED,NO_KEY
     }
 
-    static class Event {
-        private final String key;
+    static {
+        keyCodeMap.put("12", Key.W);
+        keyCodeMap.put("1f", Key.A);
+        keyCodeMap.put("20", Key.S);
+        keyCodeMap.put("21", Key.D);
+        keyCodeMap.put("3b", Key.FN);
+        keyCodeMap.put("50", Key.HOME);
+        keyCodeMap.put("00",Key.NO_KEY);
+    }
+
+    static class EventKey {
+        private final Key key;
         private final Integer value;
 
-        public Event(String key, Integer value) {
+        public EventKey(Key key, Integer value) {
             this.key = key;
             this.value = value;
         }
 
-        public String getKey() {
+        public Key getKey() {
             return key;
         }
 
@@ -52,6 +58,25 @@ public class USB {
             return "Event{" +
                     "key='" + key + '\'' +
                     ", value=" + value +
+                    '}';
+        }
+    }
+
+    static class Event {
+        private final List<EventKey> eventKeys;
+
+        Event(List<EventKey> eventKeys) {
+            this.eventKeys = eventKeys;
+        }
+
+        public List<EventKey> getEventParts() {
+            return eventKeys;
+        }
+
+        @Override
+        public String toString() {
+            return "Event{" +
+                    "eventKeys=" + eventKeys +
                     '}';
         }
     }
@@ -69,8 +94,25 @@ public class USB {
         System.out.println("waiting for input");
         while (true) {
             Event event = readEvent();
-            listener.onEvent(event);
-            System.out.println(event);
+            List<EventKey> eventParts = event.getEventParts();
+            if (eventParts.size() == 2) {
+                USB.EventKey eventKey1 = eventParts.get(0);
+                USB.EventKey eventKey2 = eventParts.get(1);
+                USB.Key key1 = eventKey1.getKey();
+                USB.Key key2 = eventKey2.getKey();
+                Integer value1 = eventKey1.getValue();
+                Integer value2 = eventKey2.getValue();
+
+                if (key1.equals(USB.Key.FN) && key2.equals(USB.Key.HOME)) {
+                    if (value1 > Short.MAX_VALUE && value2 > Short.MAX_VALUE) {
+                        System.out.println("terminating");
+                        break;
+                    }
+                }
+            }else{
+                listener.onEvent(event);
+                System.out.println(event);
+            }
         }
     }
 
@@ -78,13 +120,48 @@ public class USB {
         ByteBuffer byteBuffer = ByteBuffer.allocateDirect(HID_DATA_LENGTH);
         IntBuffer transferred = IntBuffer.allocate(1);
         int result = LibUsb.interruptTransfer(deviceHandle, ENDPOINT_IN_ANALOG, byteBuffer, transferred, 0);
-        char[] hidData = Hex.encodeHex(byteBuffer);
-        String keyCode = new String(new char[]{hidData[2], hidData[3]});
-        String valueString = new String(new char[]{hidData[4], hidData[5], hidData[6], hidData[7]});
-        String key = Optional.ofNullable(keyCodeMap.get(keyCode)).orElse(String.format("UNMAPPED:%s,hidData:%s", keyCode, new String(hidData)));
-        Integer value = Integer.decode("0x" + valueString);
+        String hidData = new String(Hex.encodeHex(byteBuffer));
+        List<EventKey> eventKeys = decode(hidData).stream().map(hidPart -> {
+            String keyCode = hidPart.getKeyCode();
+            Integer value = hidPart.getValue();
+            Key key = Optional.ofNullable(keyCodeMap.get(keyCode)).orElse(Key.UNMAPPED);
+            return new EventKey(key, value);
+        }).filter(eventKey -> !(eventKey.getKey().equals(Key.NO_KEY) && eventKey.getValue()==0))
+                .collect(Collectors.toList());
+
         checkResult(result, false);
-        return new Event(key, value);
+        return new Event(eventKeys);
+    }
+
+    private static List<HidPart> decode(String hexString) {
+        String hidParts = hexString.substring(2);
+        List<HidPart> hidPartList = new ArrayList<>(initialCapacity);
+        for (int i = 0; i < 15; i++) {
+            int offset = i * 6;
+            String hidPart = hidParts.substring(offset, offset + 6);
+            String keyCode = hidPart.substring(0, 2);
+            Integer value = Integer.decode("0x" + hidPart.substring(2, 6));
+            hidPartList.add(new HidPart(keyCode, value));
+        }
+        return hidPartList;
+    }
+
+    private static class HidPart {
+        private final String keyCode;
+        private final Integer value;
+
+        private HidPart(String keyCode, Integer value) {
+            this.keyCode = keyCode;
+            this.value = value;
+        }
+
+        public String getKeyCode() {
+            return keyCode;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
     }
 
     public static void reset() throws DecoderException {
